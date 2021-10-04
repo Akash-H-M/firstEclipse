341db3511f6b0355c7bdbae0b5e99457f33f8cd5
Akash-H-M <akashhm2000@gmail.com>
2021-10-04 18:57:03 +0530

    this is the first eclipse project with git.

---
+++ gitdemo/src/gitdemo/Firstgit.java
@@ -0,0 +1,18 @@
+package gitdemo;
+
+public class Firstgit {
+
+	public static void main(String[] args) {
+		int i=0;
+		while(i<10)
+		{
+			if(i%2==0)
+			{
+				System.out.println("hii akash!");
+			}
+			else
+				System.out.println("welcome to git...");
+		}
+	}
+
+}
